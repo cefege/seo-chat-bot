684dd5ebe0ad722eae8db057c80d384d84563a90
Mihai Mateias <mateiasmihaiandrei@gmail.com>
2023-04-28 02:31:40 +0300

    first commit

---
+++ scraper-embedder.py
@@ -0,0 +1,277 @@
+import time
+import advertools as adv
+import pandas as pd
+import tiktoken
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from uuid import uuid4
+from tqdm.auto import tqdm
+import tiktoken
+import openai
+import pinecone
+import toml
+
+# Load the secrets.toml file
+secrets = toml.load(".streamlit/secrets.toml")
+
+# Retrieve the values
+PINECONE_API_KEY = secrets["API"]["PINECONE_API_KEY"]
+OPEN_AI_API_KEY = secrets["API"]["OPEN_AI_API_KEY"]
+
+sitemaps = [
+    "https://www.seobythesea.com/post-sitemap.xml",
+    "https://www.seobythesea.com/post-sitemap2.xml",
+    "https://www.holisticseo.digital/post-sitemap1.xml",
+    "https://www.holisticseo.digital/post-sitemap2.xml",
+    "https://www.holisticseo.digital/post-sitemap3.xml",
+    "https://www.holisticseo.digital/post-sitemap4.xml",
+]
+
+additional_urls = [
+    "https://www.oncrawl.com/technical-seo/importance-topical-authority-semantic-seo/",
+    "https://www.holisticseo.digital/theoretical-seo/topical-authority/",
+    "https://inlinks.net/p/case-studies/importance-of-entity-oriented-search-understanding-for-seo-beyond-strings/",
+    "https://www.oncrawl.com/technical-seo/creating-semantic-content-networks-with-query-document-templates-case-study/",
+    "https://www.holisticseo.digital/theoretical-seo/ranking/",
+    "https://www.oncrawl.com/technical-seo/multilingual-international-seo-guidelines-case-study/",
+    "https://www.holisticseo.digital/technical-seo/multilingual-seo/",
+    "https://www.oncrawl.com/technical-seo/google-core-updates-effects-problems-and-solutions-for-ymyl-sites/",
+    "https://www.oncrawl.com/technical-seo/how-to-become-a-winner-from-every-google-core-algorithm-update/",
+    "https://jetoctopus.com/importance-of-ranking-signal/",
+    "https://serpstat.com/blog/the-importance-of-technical-seo-insights-of-technical-issues-of-unibaby-project/",
+    "https://serpstat.com/blog/user-experience-authoritative-content-and-page-speed-for-organic-traffic-growth/",
+    "https://serpstat.com/blog/how-a-web-entity-can-grow-traffic-and-lose-google-core-algorithm-update-at-the-same-time/",
+    "https://www.rootandbranchgroup.com/importance-of-context-for-seo/",
+    "https://www.holisticseo.digital/python-seo/data-science/",
+    "https://www.holisticseo.digital/theoretical-seo/google-author",
+    "https://www.authoritas.com/blog/encazip-case-study/",
+]
+
+
+def scrape_sitemaps(sitemaps):
+    """Scrape a list of sitemaps and return a dataframe with the results"""
+    df = pd.concat([adv.sitemap_to_df(sitemap) for sitemap in sitemaps])
+    df.to_csv("sitemap.csv", index=False)
+
+    return df
+
+
+def convert_df_to_list(df):
+    """Convert a dataframe to a list"""
+    return df["loc"].tolist()
+
+
+# merge url lists and remove duplicates
+def merge_url_lists(url_list1, url_list2):
+    return list(set(url_list1 + url_list2))
+
+
+def scrape_pages(url_list):
+    adv.crawl(url_list, output_file="page_data.jl", follow_links=False)
+    # convert jl to csv
+    df = pd.read_json("page_data.jl", lines=True)
+    df = df[["url", "title", "body_text"]]
+    df.to_json("page_data.json", orient="records", lines=True)
+    df.to_csv("page_data.csv", index=False)
+    return df
+
+
+def convert_df_to_dict(df):
+    df = pd.read_json(
+        "page_data.json", lines=True, orient="records", encoding="utf-8", dtype=str
+    )
+    df = df.to_dict(orient="records")
+    return df
+
+
+## Step 1
+# df = scrape_sitemaps(sitemaps)
+# url_list_1 = convert_df_to_list(df)
+# urls = merge_url_lists(url_list_1, additional_urls)
+# scrape_pages(urls)
+
+
+# create the length function
+def tiktoken_len(text):
+    tokenizer = tiktoken.get_encoding("p50k_base")
+    tokens = tokenizer.encode(text, disallowed_special=())
+    return len(tokens)
+
+
+def create_chunks(data):
+    text_splitter = RecursiveCharacterTextSplitter(
+        chunk_size=500,
+        chunk_overlap=20,
+        length_function=tiktoken_len,
+        separators=["\n\n", "\n", " ", ""],
+    )
+    chunks = []
+    for idx, record in enumerate(tqdm(data)):
+        texts = text_splitter.split_text(record["body_text"])
+        chunks.extend(
+            [
+                {
+                    "id": str(uuid4()),
+                    "title": record["title"],
+                    "text": texts[i],
+                    "chunk": i,
+                    "url": record["url"],
+                }
+                for i in range(len(texts))
+            ]
+        )
+    return chunks
+
+
+def init_pinecone(api_key, environment):
+    """
+    Initializes a connection to the Pinecone service.
+
+    Args:
+        api_key (str): The API key for the Pinecone service.
+        environment (str): The environment name for the Pinecone service.
+
+    Returns:
+        None
+    """
+    pinecone.init(api_key=api_key, environment=environment)
+
+
+def create_index_if_not_exists(index_name, dimension, metric):
+    # Check if the index already exists
+    if index_name not in pinecone.list_indexes():
+        # If the index does not exist, create a new index
+        pinecone.create_index(index_name, dimension=dimension, metric=metric)
+        print(f"Index '{index_name}' created successfully.")
+    else:
+        print(f"Index '{index_name}' already exists.")
+
+
+def create_embeddings(chunks, embed_model, index, batch_size=100):
+    """
+    Create embeddings for text chunks and insert them into Pinecone index.
+
+    Args:
+        chunks (list): List of text chunks to be embedded.
+        embed_model (str): Name of the OpenAI GPT model to use for embedding.
+        index (pinecone.Index): Pinecone index object to insert embeddings into.
+        batch_size (int): Number of embeddings to create and insert at once.
+
+    Returns:
+        None
+    """
+    # Set up OpenAI API key
+    openai.api_key = OPEN_AI_API_KEY
+
+    for i in tqdm(range(0, len(chunks), batch_size)):
+        # find end of batch
+        i_end = min(len(chunks), i + batch_size)
+        meta_batch = chunks[i:i_end]
+        # get ids
+        ids_batch = [x["id"] for x in meta_batch]
+        # get texts to encode
+        texts = [x["text"] for x in meta_batch]
+        # create embeddings (try-except added to avoid RateLimitError)
+        try:
+            res = openai.Embedding.create(input=texts, engine=embed_model)
+        except:
+            done = False
+            while not done:
+                time.sleep(5)
+                try:
+                    res = openai.Embedding.create(input=texts, engine=embed_model)
+                    done = True
+                except:
+                    pass
+        embeds = [record["embedding"] for record in res["data"]]
+        # cleanup metadata
+        meta_batch = [
+            {
+                "title": x["title"],
+                "text": x["text"],
+                "chunk": x["chunk"],
+                "url": x["url"],
+            }
+            for x in meta_batch
+        ]
+        to_upsert = list(zip(ids_batch, embeds, meta_batch))
+        # upsert to Pinecone
+        index.upsert(vectors=to_upsert)
+
+
+### STEP 2 Embedding and indexing
+
+
+# data = pd.read_json(
+#     "page_data.json", lines=True, orient="records", encoding="utf-8", dtype=str
+# )
+# # convert to list of dicts
+# data = data.to_dict(orient="records")
+
+# chunks = create_chunks(data)
+
+
+# init_pinecone(
+#     api_key=PINECONE_API_KEY, environment="us-west4-gcp"
+# )
+
+# create_index_if_not_exists(
+#     index_name="gpt-4-langchain-docs", dimension=1536, metric="dotproduct"
+# )
+
+# index = pinecone.GRPCIndex("gpt-4-langchain-docs")
+
+
+# create_embeddings(
+#     chunks=chunks,
+#     embed_model="text-embedding-ada-002",
+#     index=index,
+#     batch_size=100,
+# )
+
+
+# Step 3 Query Vector Database
+
+init_pinecone(api_key=PINECONE_API_KEY, environment="us-west4-gcp")
+
+index = pinecone.GRPCIndex("gpt-4-langchain-docs")
+
+query = "Based only on the above text, explain how to close gaps between different localities, query networks, and templates,"
+openai.api_key = OPEN_AI_API_KEY
+res = openai.Embedding.create(input=[query], engine="text-embedding-ada-002")
+
+# retrieve from Pinecone
+xq = res["data"][0]["embedding"]
+
+# get relevant contexts (including the questions)
+res = index.query(xq, top_k=10, include_metadata=True)
+
+# print(res)
+
+# Stept 4 : Augment Query
+
+# get list of retrieved text
+
+contexts = [item["metadata"]["text"] for item in res["matches"]]
+
+augmented_query = "\n\n---\n\n".join(contexts) + "\n\n-----\n\n" + query
+
+# print(augmented_query)
+
+# Step 5 : Query GPT-3.5
+# system message to 'prime' the model
+primer = f"""You are Q&A bot. A highly intelligent system that answers
+user questions based on the information provided by the user above
+each question. If the information can not be found in the information
+provided by the user you truthfully say "I don't know".
+"""
+
+import pprint
+
+res = openai.ChatCompletion.create(
+    model="gpt-3.5-turbo",
+    messages=[
+        {"role": "system", "content": primer},
+        {"role": "user", "content": augmented_query},
+    ],
+)
+pprint.pprint(res["choices"][0]["message"]["content"])
---
+++ seo-chat-st.py
@@ -0,0 +1,102 @@
+import streamlit as st
+import pinecone
+import openai
+
+
+PINECONE_API_KEY = st.secrets["API"]["PINECONE_API_KEY"]
+OPEN_AI_API_KEY = st.secrets["API"]["OPEN_AI_API_KEY"]
+
+
+def generate_output_string(list_of_dicts):
+    result_string = ""
+    for item in list_of_dicts:
+        result_string += "title: {}\nurl: {}\ntext: {}\n-----\n".format(
+            item["title"], item["url"], item["text"]
+        )
+    return result_string
+
+
+def get_relevant_contexts(query_embedding, index):
+    index = pinecone.Index(index_name=index)
+    res = index.query(query_embedding, top_k=8, include_metadata=True)
+    contexts = []
+    for item in res["matches"]:
+        metadata = item["metadata"]
+        text = metadata.get("text", "")
+        url = metadata.get("url", "")
+        title = metadata.get("title", "")
+        context = {"text": text, "url": url, "title": title}
+        contexts.append(context)
+
+    contexts = str(contexts)
+    return contexts
+
+
+def augment_query(contexts, query):
+    augmented_query = f"{contexts} \n\n-----\n\n {query}"
+    return augmented_query
+
+
+def get_query_embedding(query):
+    """
+    This function takes a query string as input and returns its embedding as computed by OpenAI's text-embedding-ada-002 engine.
+
+    Args:
+    query (str): The query to be embedded.
+
+    Returns:
+    query_embedding (list): A list containing the embedding vector of the query.
+    """
+
+    # Embed the query using OpenAI's text-embedding-ada-002 engine
+    query_embedding = openai.Embedding.create(
+        input=[query], engine="text-embedding-ada-002"
+    )["data"][0]["embedding"]
+
+    return query_embedding
+
+
+def run_chatbot(augmented_query):
+    primer = f"""
+As an AI Q&A bot, your main function is to provide detailed answers to user questions based on the information provided above each question. It is crucial to cite sources accurately by using the [[number](URL)] notation after the reference. In cases where the search results refer to multiple subjects with the same name, it is important to write separate answers for each subject. If the information cannot be found in the given data, it is important to truthfully respond with "I don't know". All answers must be written in markdown format, and it is recommended to use as much information as possible when responding to questions. Please commence your operations.
+              """
+
+    res = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=[
+            {"role": "system", "content": primer},
+            {"role": "user", "content": augmented_query},
+        ],
+    )
+
+    return res["choices"][0]["message"]["content"]
+
+
+def main():
+    st.title("SEO Q&A Chatbot")
+    st.write("Ask any question based on the given context.")
+
+    # Input field for user's question
+    query = st.text_input("Enter your question")
+
+    if query:
+        st.write(query)
+        # retrieve from Pinecone
+        query_embedding = get_query_embedding(query)
+
+        # get relevant contexts (including the questions)
+        contexts = get_relevant_contexts(query_embedding, index="gpt-4-langchain-docs")
+
+        augmented_query = augment_query(contexts, query)
+
+        # Run chatbot
+        response = run_chatbot(augmented_query)
+        st.markdown(response)
+        st.write("Raw Semantic Search Results:")
+        st.json(contexts)
+
+
+if __name__ == "__main__":
+    openai.api_key = OPEN_AI_API_KEY
+    pinecone.init(api_key=PINECONE_API_KEY, environment="us-west4-gcp")
+    main()
